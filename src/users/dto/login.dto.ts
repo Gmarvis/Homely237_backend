fe3c23b9eecd9121dc9b1e02d843698f32408990
gmarivis <sgmarvis@gmail.com>
2024-05-30 09:34:12 +0100

    added

--- src/users/dto/login.dto.ts
+++ src/users/dto/login.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsNotEmpty,
-  IsString,
-  IsEmail,
-  MinLength,
-  IsStrongPassword,
-} from 'class-validator';
+import { IsNotEmpty, IsString, IsEmail, MinLength } from 'class-validator';
 
 export class LoginDto {
   @IsEmail({}, { message: 'please enter a valid email' })
@@ -13,7 +7,6 @@ export class LoginDto {
 
   @IsString()
   @IsNotEmpty()
-  // @IsStrongPassword()
   @MinLength(6)
   readonly password: string;
 }
